1383b108d78b45548a6caf3248c1ce7f13e2cd4d
theWuukie <78221645+theWuukie@users.noreply.github.com>
2022-01-29 23:04:17 +0100

    platform gitmos

--- KuhBus/Assets/Scripts/MovingPlatform.cs
+++ KuhBus/Assets/Scripts/MovingPlatform.cs
@@ -9,14 +9,13 @@ public class MovingPlatform : MonoBehaviour
 
     [SerializeField] private Vector2 movement;
 
-    private float distance;
     private Vector2 startPosition;
+    private bool awake = false;
 
     void Awake()
     {
+        awake = true;
         startPosition = transform.position;
-        distance = movement.magnitude;
-        movement.Normalize();
     }
 
     // Start is called before the first frame update
@@ -27,14 +26,7 @@ public class MovingPlatform : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //var dir = new Vector3(maxX - minX, maxY - minY, 0);
-        //dir.Normalize();
-        //if ((horizontal && (transform.position.x > maxX || transform.position.x < minX)) || (vertical && (transform.position.y > maxY || transform.position.y < minY)))
-        //    speed *= -1.0f;
-        //transform.position += dir * speed;
-
-
-        transform.position = startPosition + distance * movement * Mathf.Sin(Time.time * speed);
+        transform.position = startPosition + movement * Mathf.Sin(Time.time * speed);
     }
 
     // Update is called once per frame
@@ -43,11 +35,14 @@ public class MovingPlatform : MonoBehaviour
         
     }
 
-  void OnDrawGizmos()
+    //[ExecuteInEditMode]
+    void OnDrawGizmos()
   {
-        //Gizmos.DrawLine(new Vector3( (horizontal?minX:transform.position.x) , (vertical ? minY : transform.position.y), 0.0f), new Vector3((horizontal ? maxX : transform.position.x), (vertical ? maxY : transform.position.y), 0.0f));
-        Vector2 start = startPosition - distance * movement;
-        Vector2 end   = startPosition + distance * movement;
+        if (!awake)
+          Awake();
+
+        Vector2 start = startPosition - movement;
+        Vector2 end   = startPosition + movement;
 
         Gizmos.DrawLine(start, end);
     }
